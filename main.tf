8c95b422ddcd83645eb69d0f0a51d144a6514bba
Martin George <itsmemartin@gmail.com>
2021-03-26 13:34:58 -0400

    new commit

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-resource "azurerm_test_deploy" "rmdeploy" {
-  name = "MyResource"
+provider "azurerm" {
+version = "~> 1.44"
+}
+
+resource "azurerm_resource_group" "rmdeploy" {
+  name = "MyResource1"
   location = "eastus"
 }
